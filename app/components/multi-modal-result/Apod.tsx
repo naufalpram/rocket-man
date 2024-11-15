1dd933485abf067e5a047775bd634fd37c18fbfe
Naufal Pramudya Yusuf <naufal.pramudya@ui.ac.id>
2024-11-15 10:38:24 +0700

    add loading state and error state for APOD image

--- app/components/multi-modal-result/Apod.tsx
+++ app/components/multi-modal-result/Apod.tsx
@@ -9,15 +9,19 @@ const APODResult = ({ result }: { result: APODResponse }) => {
         <div className='rounded-lg w-fit h-fit'>
           <Image
             alt={'title' in result ? result.title : 'Astronomy Picture of The Day'}
-            src={'url' in result ? result.url : ''}
+            src={'url' in result ? result.url : '/err_img.webp'}
             className='hero-image rounded-xl'
             width={0}
             height={0}
             sizes="responsive"
-            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            style={{ width: "100%", height: "100%", minWidth: "20rem", minHeight: "20rem", objectFit: "cover" }}
             priority
-            blurDataURL='1x1-767f7f7f.png'
+            blurDataURL={'/1x1-767f7f7f.png'}
             placeholder='blur'
+            onError={(e) => {
+              const target = e.target as HTMLImageElement;
+              target.srcset = '/err_img.webp';
+            }}
           />
         </div>
         <div className='absolute top-0 w-full h-full p-6 rounded-lg bg-black/65 opacity-0 hover:opacity-100 transition-opacity flex flex-col justify-end'>
