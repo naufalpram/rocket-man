103d15d3633961999906712f5df1ba796ec50b5a
Naufal Pramudya Yusuf <naufal.pramudya@ui.ac.id>
2025-04-15 14:37:17 +0700

    modify system message for streamText

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -15,12 +15,16 @@ export async function POST(req: Request) {
   // Access the server config
   const serverConfig = await getServerConfig();
   const modelName = serverConfig?.getString('model_name') ?? 'gemini-1.5-flash-latest';
-  const systemMessage = serverConfig?.getString('system_message') ?? 'You only answer in slang based on the language of the user\'s prompt. You\'re cool and has interest in astronomy. You can still add your knowledge on everything, not just on astronomy.';
   const { messages } = await req.json();
 
   const result = streamText({
     model: google(modelName),
-    system: systemMessage,
+    system: `
+      You\'re cool and has interest in astronomy.
+      You only answer in slang based on the language of the user\'s prompt.
+      You can still add your knowledge on everything and not just on astronomy,
+      but try to bring it back to astronomy topics if the user took the convo outside your original task.
+    `,
     messages,
     experimental_transform: smoothStream(),
     experimental_activeTools: ['astronomyPictureOfTheDay', 'getNaturalEventType', 'naturalEventsShowcase'],
